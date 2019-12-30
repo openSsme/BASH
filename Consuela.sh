581146892631d8e16b7f74dd6e4efbd2e533b923
404 <openssme@gmail.com>
2019-12-30 20:54:08 +0200

    updated --ayuda command and condition additional minor change

--- Consuela.sh
+++ Consuela.sh
@@ -13,16 +13,16 @@ declare -a PARAMS
 PARAMS+=("--limpiar" "--ayuda" "--nuell")
 
 Limpiar(){
-	DIRT_SIZE=`ls -og "$1" |awk '{print $3'}`
-	echo -e "\nok i clean $1 - $DIRT_SIZE now\n"
+	SIZE=`ls -og "$1" |awk '{print $3'}`
+	echo -e "\nok i clean $1 - $SIZE now\n"
 	sync
-	dd if=/dev/zero of="$1" bs=$DIRT_SIZE count=1 iflag=fullblock 2>/dev/null
+	dd if=/dev/zero of="$1" bs=$SIZE count=1 iflag=fullblock 2>/dev/null
 	sync
 	echo "Wipe 1"
-	tr '\0' '1' < /dev/zero |dd of="$1" bs=$DIRT_SIZE count=1 iflag=fullblock 2>/dev/null
+	tr '\0' '1' < /dev/zero |dd of="$1" bs=$SIZE count=1 iflag=fullblock 2>/dev/null
 	sync
 	echo "Wipe 2"
-	dd if=/dev/urandom of="$1" bs=$DIRT_SIZE count=1 iflag=fullblock 2>/dev/null
+	dd if=/dev/urandom of="$1" bs=$SIZE count=1 iflag=fullblock 2>/dev/null
 	sync
 	echo "Wipe 3"
 	rm -f "$1"
@@ -65,9 +65,9 @@ then
 	exit $E_NOROOT
 fi
 
-if [[ $# -ne $ARGS || $1 = ${PARAMS[1]} ]]
+if [[ $# -ne $ARGS && $1 != ${PARAMS[1]} ]]
 then
-	echo "Usage: `basename $0` ${PARAMS[0]} [this] : clean something, ${PARAMS[1]} : show help"
+	echo "Usage: Consuela [--command] [option], --ayuda shows help"
 	exit $E_WRONGARGS
 fi
 
@@ -83,13 +83,23 @@ then
 	fi
 fi
 
+if [ $1 = ${PARAMS[1]} ]
+then
+	echo -e "\nConsuela is here to clean your mess"
+	echo -e "Consuela utilizes lemon pledge (dd) for securely cleaning your mess\n"
+	echo -e "\nCOMMANDS:"
+	echo -e "	--limpiar [file/directory name]	:	securely wipe a single file or recursively wipe the contents of a directory"
+	echo -e "	--nuell [block size]		:	creates 10 random files with a specified block size"
+	echo -e "	--ayuda				:	show this screen\n"
+fi
+
 if [ $1 = ${PARAMS[2]} ]
 then
-	DIRT_SIZE=$2
+	SIZE=$2
 	for (( i=0;i<10;i++ ))
 	do
 		MC=`mcookie`
-		dd if=/dev/urandom of=$MC bs=$DIRT_SIZE count=1 iflag=fullblock 2>/dev/null
-		echo "Hice un desastre :} - $MC : $DIRT_SIZE"
+		dd if=/dev/urandom of=$MC bs=$SIZE count=1 iflag=fullblock 2>/dev/null
+		echo "Hice un desastre :} - $MC : $SIZE"
 	done
 fi
