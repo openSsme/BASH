88f3a520a6847231d927eca3b5366ebd49004f41
404 <openssme@gmail.com>
2019-12-30 22:41:40 +0200

    minor update

--- Consuela.sh
+++ Consuela.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Consuela is here to clean your mess
-## Consuela utilizes lemon pledge (dd) for securely cleaning your mess
+## Consuela utilizes lemon pledge (dd) for best cleaning results
 
 ROOT_UID=0
 E_NOROOT=13
@@ -9,9 +9,8 @@ E_WRONGARGS=22
 E_NO_SUCH_FILE=2
 ARGS=2
 COUNTER=0
-declare -a PARAMS
-PARAMS+=("--limpiar" "--ayuda" "--nuell")
-
+PARAMS=("--limpiar" "--descanso" "--ayuda")
+#------------------------------------------------------------------------------
 Limpiar(){
 	SIZE=`ls -og "$1" |awk '{print $3'}`
 	echo -e "\nok i clean $1 - $SIZE now\n"
@@ -59,13 +58,15 @@ Consuela(){
 	fi
 }
 
+#------------------------------------------------------------------------------
+
 if [ $UID -ne $ROOT_UID  ]
 then
 	echo "\nno.. mister root no home :|\n"
 	exit $E_NOROOT
 fi
 
-if [[ $# -ne $ARGS && $1 != ${PARAMS[1]} ]]
+if [[ $# -ne $ARGS && $1 != ${PARAMS[2]} ]]
 then
 	echo "Usage: Consuela [--command] [option], --ayuda shows help"
 	exit $E_WRONGARGS
@@ -85,21 +86,29 @@ fi
 
 if [ $1 = ${PARAMS[1]} ]
 then
-	echo -e "\nConsuela is here to clean your mess"
-	echo -e "Consuela utilizes lemon pledge (dd) for securely cleaning your mess\n"
-	echo -e "\nCOMMANDS:"
-	echo -e "	--limpiar [file/directory name]	:	securely wipe a single file or recursively wipe the contents of a directory"
-	echo -e "	--nuell [block size]		:	creates 10 random files with a specified block size"
-	echo -e "	--ayuda				:	show this screen\n"
-fi
-
-if [ $1 = ${PARAMS[2]} ]
-then
 	SIZE=$2
 	for (( i=0;i<10;i++ ))
 	do
 		MC=`mcookie`
 		dd if=/dev/urandom of=$MC bs=$SIZE count=1 iflag=fullblock 2>/dev/null
-		echo "Hice un desastre :} - $MC : $SIZE"
+		DETAILS=`ls -hog $MC`
+		echo "This was already broken :|	: $DETAILS"
 	done
+fi
+
+if [ $1 = ${PARAMS[2]} ]
+then
+	echo -e "\nConsuela is here to clean your mess"
+	echo -e "Consuela utilizes lemon pledge (dd) for best cleaning results\n"
+	echo -e "\nCOMMANDS:"
+	echo -e "	--limpiar <path/target>		:	securely wipe a single file or recursively wipe the contents of a directory"
+	echo -e "	--descanso <block size>		:	creates 10 random files with a specified block size"
+	echo -e "	--ayuda				:	show this screen\n"
+	echo -e "EXAMPLES:"
+	echo -e "	Clean a single file inside a directory:"
+	echo -e "		Consuela --limpiar Dir1/Dir2/ab47b5e2c4b4d7ea4a19feaa71084d88.iso\n"
+	echo -e "	Clean an entire directory and its sub-directories:"
+	echo -e "		Consuela --limpiar Dir1/Dir2\n"
+	echo -e "	Create 10 files of 100M worth of testing junk in current directory:"
+	echo -e "		Consuela --descanso 100M\n"
 fi
